da3fb9983d0ea980c8dbeeff20337ae53aa8b539
prathum08 <prathumbhangadia08@gmail.com>
2025-05-31 23:38:10 +0530

    u

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='text-7xl text-amber-300 flex justify-center bg-cyan-300'>HEllo Everyone</h1>
+      <h1 className='text-7xl text-amber-300 flex justify-center bg-cyan-300'>HEllo my name is Prathum Bhangadia</h1>
     </div>
   )
 }
