4f3d4f0d9854bf44e6694c2d8380b192ba5b4769
prathum08 <prathumbhangadia08@gmail.com>
2025-06-10 23:31:53 +0530

    Login Page

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -21,6 +21,7 @@ import pic3 from './pic3.png'
 import pic4 from './pic4.png'
 import img1 from './img1.jpg'
 import img2 from './img2.jpg'
+import logo_big2 from './logo_big2.svg'
 
 const assets = {
     avatar_icon,
@@ -35,7 +36,8 @@ const assets = {
     arrow_icon,
     code,
     bgImage,
-    profile_martin
+    profile_martin,
+    logo_big2
 }
 
 export default assets;
@@ -48,35 +50,35 @@ export const userDummyData = [
         "email": "test1@greatstack.dev",
         "fullName": "Alison Martin",
         "profilePic": profile_alison,
-        "bio": "Hi Everyone, I am Using QuickChat",
+        "bio": "Hi Everyone, I am Using SyncRoom",
     },
     {
         "_id": "680f50e4f10f3cd28382ecf9",
         "email": "test2@greatstack.dev",
         "fullName": "Martin Johnson",
         "profilePic": profile_martin,
-        "bio": "Hi Everyone, I am Using QuickChat",
+        "bio": "Hi Everyone, I am Using SyncRoom",
     },
     {
         "_id": "680f510af10f3cd28382ed01",
         "email": "test3@greatstack.dev",
         "fullName": "Enrique Martinez",
         "profilePic": profile_enrique,
-        "bio": "Hi Everyone, I am Using QuickChat",
+        "bio": "Hi Everyone, I am Using SyncRoom",
     },
     {
         "_id": "680f5137f10f3cd28382ed10",
         "email": "test4@greatstack.dev",
         "fullName": "Marco Jones",
         "profilePic": profile_marco,
-        "bio": "Hi Everyone, I am Using QuickChat",
+        "bio": "Hi Everyone, I am Using SyncRoom",
     },
     {
         "_id": "680f516cf10f3cd28382ed11",
         "email": "test5@greatstack.dev",
         "fullName": "Richard Smith",
         "profilePic": profile_richard,
-        "bio": "Hi Everyone, I am Using QuickChat",
+        "bio": "Hi Everyone, I am Using SyncRoom",
     }
 ]
 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,14 +6,14 @@ import ChatContainer from '../components/ChatContainer'
 
 const HomePage = () => {
 
-    const [selectUser , setSelectUser] = useState(false)
+    const [selectedUser , setSelectedUser] = useState(false)
     
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-      <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectUser ? 'md:grid-cols-fr-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
-        <SideBar />
-        <ChatContainer />
-        <RightSidebar />
+      <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-fr-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
+        <SideBar selectedUser={selectedUser} setselectedUser={setSelectedUser}/>
+        <ChatContainer selectedUser={selectedUser} setselectedUser={setSelectedUser} />
+        <RightSidebar selectedUser={selectedUser} setselectedUser={setSelectedUser} />
       </div>
     </div>
   )
