582550f39037b999a887cc9c1c47604280ee9980
prathum08 <prathumbhangadia08@gmail.com>
2025-06-13 21:35:07 +0530

    Server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import http from "http";
+
+const app = express();
+const server = http.createServer(app)
+
+//Middleware
+app.use(express.json({limit: "4mb"}));
+app.use(cors());
+
+app.get("/api/status" ,(req ,res) => res.send("Server is live"))
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT , () => console.log("Server is running on PORT: " + PORT));
